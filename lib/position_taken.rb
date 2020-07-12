972b9ec671395db6c85e0d82bad19bc73a5c9af0
rational-architecture-1641 <jarmah241@gmail.com>
2020-07-12 23:20:29 +0000

    Automatically backed up by Learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -3,7 +3,7 @@
 def position_taken?(board, index)
   if board[index] == " " or "" or nil
     false
-  elsif board[index] == "X" or "O"
+  elsif board[index] == "X" or board[index] == "O"
     
   end
 end
