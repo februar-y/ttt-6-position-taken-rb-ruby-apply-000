be81c53d7b6b69d4196618296bfa407ae29589a9
rational-architecture-1641 <jarmah241@gmail.com>
2020-07-12 23:21:08 +0000

    Automatically backed up by Learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,9 +1,9 @@
 # code your #position_taken? method here!
 
 def position_taken?(board, index)
-  if board[index] == " " or "" or nil
+  if board[index] == " " or board[index] == "" or board[index] == nil
     false
-  elsif board[index] == "X" or board[index] == "O"
+  elsif board[index] == "X" or "O"
     true
   end
 end
